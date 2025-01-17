72bce9057667a84662fcbebb5ed824b9da602de9
JoshFost <121299124+JoshFost@users.noreply.github.com>
2025-01-17 11:31:14 +0000

    Update main.py
    added if else for menu choice 1. If inventory is empty return "inventory empty"

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 print ("Inventory Management Menu")
+print ("print ("Inventory Management Menu")
 print ("1. View Inventory ")
+print ("2. Add an item ")
+print ("3. Edit an item")
+print ("4. Exit")
+menu_choice = int(input("Please select a number for the menu item you want.(1-4)\n"))
+print(menu_choice)
+#add inventory
+inventory = []
+if menu_choice == 1:
+    if len(inventory) == 0:
+        print("Inventory is empty")
+    else:
+        print(inventory)1. View Inventory ")
 print ("2. Add an item ")
 print ("3. Edit an item")
 print ("4. Exit")
